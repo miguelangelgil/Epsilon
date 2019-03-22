919b434c05e22cde7487554b84282b6e15d85995
miguelangelgil <36223494+miguelangelgil@users.noreply.github.com>
2019-03-22 10:53:31 +0100

    base proyecto
    proyecto emty

---
+++ app/src/main/java/com/puerto7070/epsilon/MainActivity.java
@@ -0,0 +1,13 @@
+package com.puerto7070.epsilon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
