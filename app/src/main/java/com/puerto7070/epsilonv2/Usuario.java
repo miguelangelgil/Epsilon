f38a9d1488c719307f172707440e20a9effd0506
miguelangelgil <36223494+miguelangelgil@users.noreply.github.com>
2019-03-23 07:10:23 +0100

    usuario
    añado funciones utiles

--- app/src/main/java/com/puerto7070/epsilonv2/Usuario.java
+++ app/src/main/java/com/puerto7070/epsilonv2/Usuario.java
@@ -4,11 +4,23 @@ public class Usuario {
     private String nombre;
     private String clave;
     private String correo;
+    private int minutos;
+    private long horas;
     public Usuario(String nombre, String clave, String correo)
     {
         this.nombre = nombre;
         this.clave = clave;
         this.correo = correo;
+
+    }
+
+    public Usuario(String nombre, String clave, String correo, long horas, int minutos)
+    {
+        this.nombre = nombre;
+        this.clave = clave;
+        this.correo = correo;
+        this.horas = horas;
+        this.minutos = minutos;
 
     }
     public boolean Check_nombre(String nombre)
@@ -31,5 +43,31 @@ public class Usuario {
     public String Get_correo()
     {
         return correo;
+    }
+    public int Get_minutos()
+    {
+        return minutos;
+    }
+    public long Get_horas()
+    {
+        return horas;
+    }
+    public void Sumar_minutos(int minutos)
+    {
+        this.minutos += minutos;
+    }
+    public void Sumar_horas(int horas)
+    {
+        this.horas += horas;
+    }
+    public void Set_tiempo(long horas, int minutos)
+    {
+        this.horas = horas;
+        this.minutos = minutos;
+    }
+    public void Reset_tiempo()
+    {
+        minutos=0;
+        horas=0;
     }
 }
